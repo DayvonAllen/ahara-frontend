5fe89321e11eb40b5840b3f0e1a4bc92b51055ea
DayvonAllen <dayvonallen93@outlook.com>
2021-08-01 23:25:48 -0500

    fixed styling

--- pages/categories/page/[category].js
+++ pages/categories/page/[category].js
@@ -11,7 +11,7 @@ export default function CategoryResults({ foundPosts, categories }) {
       <div
         className={`relative ${
           foundPosts?.length > 0 ? "bg-gray-50" : "bg-white"
-        } pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8`}
+        } pt-16 pb-20 px-4 sm:px-6 lg:pt-4 lg:pb-28 lg:px-8`}
       >
         <div className="absolute inset-none">
           <div className="bg-white h-1/3 sm:h-2/3" />
