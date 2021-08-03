f4532f4649bedacfa7052fb3ca7a38ee06e11c77
DayvonAllen <dayvonallen93@outlook.com>
2021-08-03 17:26:11 -0500

    fixed styling

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
 import { CameraIcon } from "@heroicons/react/solid";
 import { API_URL } from "../config/index";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function About({ categories }) {
   return (
@@ -14,13 +16,10 @@ export default function About({ categories }) {
         <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
           <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
-            <div>
-              <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-                Case Study
-              </h2>
-              <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Meet Whitney
-              </h3>
+            <div className="border-gray-600 ">
+              <h1 className="text-2xl text-gray-700 font-light tracking-wide uppercase">
+                アバウト{" "}
+              </h1>
             </div>
           </div>
           <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
@@ -28,75 +27,33 @@ export default function About({ categories }) {
               <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
                 <figure>
                   <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
-                    <img
+                    <Image
                       className="rounded-lg shadow-lg object-cover object-center"
-                      src="https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80"
-                      alt="Whitney leaning against a railing on a downtown street"
+                      src="https://res.cloudinary.com/df1dxokhm/image/upload/v1628028884/ahara_pink_00ce14ccb6.png"
+                      alt="ahara logo"
                       width={1184}
                       height={1376}
                     />
                   </div>
-                  <figcaption className="mt-3 flex text-sm text-gray-500">
-                    <CameraIcon
-                      className="flex-none w-5 h-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-2">Photograph by Marcus O’Leary</span>
-                  </figcaption>
                 </figure>
               </div>
             </div>
             <div className="mt-8 lg:mt-0">
               <div className="text-base max-w-prose mx-auto lg:max-w-none">
                 <p className="text-lg text-gray-500">
-                  Sagittis scelerisque nulla cursus in enim consectetur quam.
-                  Dictum urna sed consectetur neque tristique pellentesque.
-                  Blandit amet, sed aenean erat arcu morbi.
+                  アハラキッチンはアメリカの暮らし、レストランで働く仕事事情、国際結婚、ビザの事などを書いています。
                 </p>
               </div>
               <div className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
                 <p>
-                  Sollicitudin tristique eros erat odio sed vitae, consequat
-                  turpis elementum. Lorem nibh vel, eget pretium arcu vitae.
-                  Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                  2012年にカリフォルニアロサンゼルスに移住し、現在はテキサスで寿司シェフをしています。
                 </p>
-                <p>
-                  Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique
-                  sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum
-                  et et et sit. Faucibus sed non gravida lectus dignissim
-                  imperdiet a.
-                </p>
-                <p>
-                  Dictum magnis risus phasellus vitae quam morbi. Quis lorem
-                  lorem arcu, metus, egestas netus cursus. In.
-                </p>
-                <ul>
-                  <li>Quis elit egestas venenatis mattis dignissim.</li>
-                  <li>
-                    Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-                  </li>
-                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-                </ul>
-                <p>
-                  Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                  tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                  semper. Pellentesque in venenatis vestibulum consectetur nibh
-                  id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi
-                  enim fermentum lacus in. Viverra.
-                </p>
-                <h3>How we helped</h3>
-                <p>
-                  Tincidunt integer commodo, cursus etiam aliquam neque, et.
-                  Consectetur pretium in volutpat, diam. Montes, magna cursus
-                  nulla feugiat dignissim id lobortis amet. Laoreet sem est
-                  phasellus eu proin massa, lectus. Diam rutrum posuere donec
-                  ultricies non morbi. Mi a platea auctor mi.
-                </p>
-                <p>
-                  Sagittis scelerisque nulla cursus in enim consectetur quam.
-                  Dictum urna sed consectetur neque tristique pellentesque.
-                  Blandit amet, sed aenean erat arcu morbi.
-                </p>
+                <a
+                  href="https://youtube.com/channel/UCpsip3Vu-xLIm-eqUUjB59A"
+                  target="_blank"
+                >
+                  You Tubeではアメリカのお寿司などを紹介しています。
+                </a>
               </div>
             </div>
           </div>
